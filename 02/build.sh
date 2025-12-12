44f6c751e8a7672e956f528eebac94c755019c3e
Mark Brudnak <malibrud@gmail.com>
2025-12-11 19:44:28 -0500

    build with debug info

--- 02/build.sh
+++ 02/build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [ -f part1.odin ]; then
-    odin build part1.odin -file
+    odin build part1.odin -file -debug
 fi
 
 if [ -f part2.odin ]; then
-    odin build part2.odin -file
+    odin build part2.odin -file -debug
 fi
